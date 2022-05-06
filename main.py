4a707421afa53b6718141267faeec7f8884f04f4
KarenJ-17 <kdelgado328@unab.edu.co>
2022-05-06 15:04:46 -0500

    Primer comit python

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests 
+import json 
+ciudades={}
+url:str="http://api.openweathermap.org/data/2.5/weather?"
+ciudad=input("ciudad: ")
+lon=float(input("longitud: "))
+lat=float(input("latitud: "))
+temp=float(input("temperatura: "))
+arg={
+    'nombre':ciudad,
+    'longitud': lon,
+    'latitud': lat,
+    'temperatura': temp
+}
+response= requests.post(url,data=arg)
+print(response.text)
+
